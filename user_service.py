99d9ce0a3efe2661693eb4cca647152654ac64e8
Darysha <112837727+Darya778@users.noreply.github.com>
2024-05-29 19:08:37 +0800

    Update user_service.py

--- user_service.py
+++ user_service.py
@@ -1,6 +1,26 @@
 import time
 import hashlib
 from fastapi import FastAPI
+import logging
+import socket
+
+# Logger configuration
+logging.basicConfig(level=logging.DEBUG)
+
+class CustomLogger(logging.Logger):
+    def makeRecord(self, *args, kwargs):
+        record = super(CustomLogger, self).makeRecord(*args, kwargs)
+        record.hostname = socket.gethostname()
+        return record
+
+logger = CustomLogger("User_Service")
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - [%(hostname)s] - %(message)s'))
+logger.addHandler(console_handler)
+
+file_handler = logging.FileHandler('user_service_logs.log')  # Добавляем FileHandler для записи в файл
+file_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - [%(hostname)s] - %(message)s'))
+logger.addHandler(file_handler)
 
 app = FastAPI()
 
@@ -10,6 +30,6 @@ def hash_user_info(user_info):
 @app.get("/get_unique_id")
 def get_unique_id():
     user_id = hash_user_info(str(time.time()))  # Хэшируем уникальный идентификатор
-    print(f"User with unique ID {user_id} registered.")
+    logger.info(f"User with unique ID {user_id} registered.")
 
     return {"user_id": user_id}
