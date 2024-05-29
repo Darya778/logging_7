b0e323b4657b29885aaf435d59909c6cf15440c4
Darysha <112837727+Darya778@users.noreply.github.com>
2024-05-30 01:22:57 +0800

    Update publisher.py

--- publisher.py
+++ publisher.py
@@ -1,30 +1,49 @@
+import random
+import time
 import paho.mqtt.client as mqtt_client
 import logging
-import socket
+import requests
+from fastapi import FastAPI
+app = FastAPI()
 
-# Logger configuration
-logging.basicConfig(level=logging.DEBUG)
-
-class CustomLogger(logging.Logger):
-    def makeRecord(self, *args, kwargs):
-        record = super(CustomLogger, self).makeRecord(*args, kwargs)
-        record.hostname = socket.gethostname()
-        return record
-
-logger = CustomLogger("Publisher")
+logger = logging.getLogger("Publisher")
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler('publisher.log')
+file_handler.setLevel(logging.DEBUG)
 console_handler = logging.StreamHandler()
-console_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - [%(hostname)s] - %(message)s'))
-logger.addHandler(console_handler)
-
-file_handler = logging.FileHandler('publisher_logs.log')  # Добавляем FileHandler для записи в файл
-file_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - [%(hostname)s] - %(message)s'))
+console_handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+console_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
+logger.addHandler(console_handler)
+common_file_handler = logging.FileHandler('common.log')
+common_file_handler.setLevel(logging.DEBUG)
+common_file_handler.setFormatter(formatter)
+logger.addHandler(common_file_handler)
+def register_user():
+    response = requests.get("http://localhost:8000/get_unique_id")
+    user_id = response.json()["user_id"]
+    return user_id
 
 broker = "broker.emqx.io"
+pub_client = mqtt_client.Client(register_user())
 
-pub_client = mqtt_client.Client()  # Перемещаем создание клиента за пределы функции
-pub_client.connect(broker)  # Устанавливаем подключение к брокеру
+print("Connecting to broker", broker)
+print(pub_client.connect(broker))
+logger.debug("Connected to broker")
+pub_client.loop_start()
+print("Publishing")
+logger.info("Started publishing")
 
-def publish_message(message):
-    pub_client.publish("lab/leds/state", message)
-    logger.info(f"Published message: {message}")
+for i in range(10):
+    state = "on" if random.randint(0, 1) == 0 else "off"
+    state = state + "ArtemV"
+    logger.debug(f"Published message: {state}")
+    print(f"State is {state}")
+    pub_client.publish("lab/leds/state", state)
+    time.sleep(2)
+
+pub_client.disconnect()
+pub_client.loop_stop()
+logger.debug("Disconnected from broker")
