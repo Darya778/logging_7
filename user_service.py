d62f85b15ccc177f6f001eee96c26da8b79e37b6
Darysha <112837727+Darya778@users.noreply.github.com>
2024-05-30 01:23:18 +0800

    Update user_service.py

--- user_service.py
+++ user_service.py
@@ -2,41 +2,30 @@ import time
 import hashlib
 from fastapi import FastAPI
 import logging
-import socket
 
-# Logger configuration
-logging.basicConfig(level=logging.DEBUG)
-
-class CustomLogger(logging.Logger):
-    def makeRecord(self, name, level, fn, lno, msg, args, exc_info, func=None,
-                    extra=None, sinfo=None, **kwargs):  # Добавлен **kwargs
-        record = super(CustomLogger, self).makeRecord(
-            name, level, fn, lno, msg, args, exc_info, func, extra, sinfo, **kwargs
-        )
-        record.hostname = socket.gethostname()
-        return record
-
-logger = CustomLogger("User_Service")
+logger = logging.getLogger('User_Service')
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler('user_service.log')
+file_handler.setLevel(logging.DEBUG)
 console_handler = logging.StreamHandler()
-console_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - [%(hostname)s] - %(message)s'))
-logger.addHandler(console_handler)
-
-file_handler = logging.FileHandler('user_service_logs.log')  # Добавляем FileHandler для записи в файл
-file_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - [%(hostname)s] - %(message)s'))
+console_handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+console_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
+logger.addHandler(console_handler)
+common_file_handler = logging.FileHandler('common.log')
+common_file_handler.setLevel(logging.DEBUG)
+common_file_handler.setFormatter(formatter)
+logger.addHandler(common_file_handler)
 
 app = FastAPI()
-
-@app.get("/")  # Добавляем корневой обработчик
-async def root():
-    return {"message": "User Service is running"}
 
 def hash_user_info(user_info):
     return hashlib.md5(user_info.encode()).hexdigest()
 
 @app.get("/get_unique_id")
-def get_unique_id():
-    user_id = hash_user_info(str(time.time()))  # Хэшируем уникальный идентификатор
+async def get_unique_id():
+    user_id = hash_user_info(str(time.time()))
     logger.info(f"User with unique ID {user_id} registered.")
-
     return {"user_id": user_id}
